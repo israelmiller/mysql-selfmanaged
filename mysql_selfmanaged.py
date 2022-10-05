3a292b8d3d133a730cb7ae0db158dcda602cb510
Israel Miller <israel.miller@stonybrook.edu>
2022-10-05 19:44:29 -0400

    created script to upload data to mysql database.

---
+++ mysql_selfmanaged.py
@@ -0,0 +1,29 @@
+##import SQLalchemy and pa
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+MYSQL_HOSTNAME = os.getenv("MYSQL_HOSTNAME")
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+#Create the engine
+engine = create_engine(f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}')
+
+#define a query
+query = """SELECT * FROM temp.table1;"""
+
+#read the query into a dataframe
+df = pd.read_sql(query, con=engine)
+
+#prepare a table to upload a dataframe to a mysql table
+test_df = pd.read_csv('patients.csv')
+test_df_small = test_df.head(10)
+
+#upload to the database
+test_df_small.to_sql('patients', con=engine, if_exists='replace')
